c3eb8c96732f52a3370c44f26d92ed450b2548b6
mbraga-s <mbraga-s@student.42lisboa.com>
2023-01-09 12:14:06 +0000

    9 Jan 12h14

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,20 @@
 /*   By: mbraga-s <mbraga-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 19:31:41 by mbraga-s          #+#    #+#             */
-/*   Updated: 2022/12/26 19:31:41 by mbraga-s         ###   ########.fr       */
+/*   Updated: 2023/01/09 12:11:52 by mbraga-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	GETNEXTLINE_H
-# define GETNEXTLINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include <limits.h>
 
-char	*get_next_line (int fd);
+char	*get_next_line(int fd);
 
 int		phrase_len(char *str, int i);
 
